9851c33017ae05a9ed89055f4d8bdbc4759f1633
Mehmet BEBEK <bmbebek@gmail.com>
2020-02-04 22:18:33 +0300

    added send.js

---
+++ send.js
@@ -0,0 +1,7 @@
+var push = require('web-push');
+
+let keys = { publicKey:
+    'BGryAqQbOJM0Hh3mRJDyrOh5aN2Pe__p15iip1qpOi_xgaOutc5qNldC0Y1DS5ol5Q2lP0JPjJih9P38EA-e7Ps',
+   privateKey: 'y03FZhM5LRsdHVPBkBbhECiL6drFLMLDrREErOel-rU' };
+
+console.log(keys);
